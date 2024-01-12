95ebfb3e5a0e4065f6c76d7e1973b6fd13131ae1
DewaSRY <sdewa6645@gmail.com>
2024-01-12 20:01:26 +0800

    add extra typing on feather

--- src/libs/pokemon/FetchPokemonByName/index.ts
+++ src/libs/pokemon/FetchPokemonByName/index.ts
@@ -1,7 +1,9 @@
-import GetPokemon from "@libs/pokemon/GetPokemon";
+import GetPokemon, { GetPokemonProps } from "@libs/pokemon/GetPokemon";
 import { GetAllResult } from "@libs/pokemon/GetAllPokemon";
 
-export default async function FetchPokemonByName(pokemonNames: GetAllResult[]) {
+export default async function FetchPokemonALlByName(
+  pokemonNames: GetAllResult[]
+): Promise<GetPokemonProps[]> {
   try {
     const mapPokemon = pokemonNames.map(async (p) => await GetPokemon(p.name));
     const result = await Promise.all(mapPokemon);
