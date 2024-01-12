4d6f81aebf950a2fa2e73f3b330fb2f42b565d5f
DewaSRY <sdewa6645@gmail.com>
2024-01-12 15:38:54 +0800

    improve little bit ui

--- src/pages/navigation/index.tsx
+++ src/pages/navigation/index.tsx
@@ -13,19 +13,19 @@ const index: indexComponents = ({ ...resProps }) => {
         className={
           "  bg-white rounded-xl " +
           "w-full relative z-20  max-w-[1800px] mx-auto " +
-          "flex flex-wrap " +
+          "flex flex-wrap xl:flex-nowrap " +
           `${resProps.className ? resProps.className : ""}`
         }
       >
-        <Link to="/" className="w-[250px] ">
+        <Link to="/" className="w-[250px] xl:flex-1 ">
           <img
-            className="h-[75px] inline-block "
+            className="h-[75px] inline-block  "
             height={50}
             src={POKEDEX}
             alt=""
           />
         </Link>
-        <div className="h-full py-4 min-w-[600px] flex-1 ">
+        <div className="h-full py-4 min-w-[600px] flex-1 xl:flex-[3] ">
           <SearchPokemon className=" " />
         </div>
       </nav>
